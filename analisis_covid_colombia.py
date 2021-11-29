f1f8b942f54dd69f67696b642e7e4e8119d5c5f5
Carlos Armando De Castro <92470129+cadecastro@users.noreply.github.com>
2021-11-29 11:02:24 -0500

    Add files via upload
    Adición de nombre del depto. escogido a los títulos de sus respectivas gráficas.

--- analisis_covid_colombia.py
+++ analisis_covid_colombia.py
@@ -10,12 +10,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 #Importar datos de Datos Abiertos Colombia:
 columnas=['Nombre departamento','Edad','Sexo','Estado','Fecha de muerte']
-datos=pd.read_csv('https://www.datos.gov.co/api/views/gt2j-8ykr/rows.csv',usecols=columnas)
+covid=pd.read_csv('https://www.datos.gov.co/api/views/gt2j-8ykr/rows.csv',usecols=columnas)
 #Convertir fechas de muerte a formato adecuado:
-datos["Fecha de muerte"] = pd.to_datetime(datos["Fecha de muerte"],dayfirst=True)
+covid["Fecha de muerte"] = pd.to_datetime(covid["Fecha de muerte"],dayfirst=True)
 #COLOMBIA:
 #Casos que fallecieron por COVID-19:
-muertes=datos[datos['Estado']=='Fallecido']
+muertes=covid[covid['Estado']=='Fallecido']
 #Conteo de muertes por fecha de ocurrencia:
 fecha_muerte=muertes['Fecha de muerte'].value_counts()
 fecha_muerte=fecha_muerte.sort_index()
@@ -25,15 +25,15 @@ sexo_muerte=sexo_muerte.sort_values(by=['Fecha de muerte'],ascending=False)
 #Muertes por edad:
 edades_muerte=muertes.groupby(['Edad'])[['Fecha de muerte']].count()
 #Casos por edad:
-edades_casos=datos.groupby(['Edad'])[['Edad']].count()
+edades_casos=covid.groupby(['Edad'])[['Edad']].count()
 #CFR por edad:
 CFR_edad=edades_muerte['Fecha de muerte'].divide(edades_casos['Edad'])
 #Estadísticas casos por edad:
-stat_edad_casos=datos['Edad'].describe()
+stat_edad_casos=covid['Edad'].describe()
 stat_edad_muertes=muertes['Edad'].describe()
 #BOGOTÁ:
 #Casos y muertes:
-casos_bog=datos[datos['Nombre departamento']=='BOGOTA']
+casos_bog=covid[covid['Nombre departamento']=='BOGOTA']
 muertes_bog=casos_bog[casos_bog['Estado']=='Fallecido']
 #Conteo de muertes por fecha de ocurrencia:
 fecha_muerte_bog=muertes_bog['Fecha de muerte'].value_counts()
@@ -127,7 +127,7 @@ print('Tasa de letalidad por caso =',np.format_float_positional(stat_edad_muerte
 #DEPARTAMENTO ADICIONAL A ANALIZAR:
 depto=str(input('Departamento o distrito adicional a analizar:'))
 #Casos y muertes:
-casos_dep=datos[datos['Nombre departamento']==depto]
+casos_dep=covid[covid['Nombre departamento']==depto]
 muertes_dep=casos_dep[casos_dep['Estado']=='Fallecido']
 #Conteo de muertes por fecha de ocurrencia:
 fecha_muerte_dep=muertes_dep['Fecha de muerte'].value_counts()
@@ -153,7 +153,7 @@ print('Tasa de letalidad por caso =',np.format_float_positional(stat_edad_muerte
 plt.figure(8)
 plt.bar(fecha_muerte_dep.index,fecha_muerte_dep,color='blue')
 fecha_muerte_dep.rolling(window =7).mean().plot(color='red')
-plt.title('Muertes diarias COVID-19 departamento',loc='left')
+plt.title('Muertes diarias COVID-19 '+depto,loc='left')
 plt.title('cadecastro.com',loc='right')
 plt.grid(True,'both','both')
 plt.ylim(0,None)
@@ -162,7 +162,7 @@ plt.legend(['Media semanal','Datos'])
 plt.figure(9)
 plt.fill_between(edades_casos_dep.index,edades_casos_dep['Edad'],color='blue')
 plt.fill_between(edades_muerte_dep.index,edades_muerte_dep['Fecha de muerte'],color='red')
-plt.title('Casos y muertes COVID-19 por edad Depto. seleccionado',loc='left')
+plt.title('Casos y muertes COVID-19 por edad '+depto,loc='left')
 plt.grid(True,'both','both')
 plt.legend(['Casos','Muertes'])
 plt.xlim(1,114)
@@ -171,7 +171,7 @@ plt.xlabel('cadecastro.com')
 #Gráfica CFR Depto:
 plt.figure(10)
 plt.plot(CFR_dep,'b')
-plt.title('Letalidad por caso por edad Depto. seleccionado',loc='left')
+plt.title('Letalidad por caso por edad '+depto,loc='left')
 plt.title('cadecastro.com',loc='right')
 plt.grid(True,'both','both')
 plt.legend(['CFR'])
