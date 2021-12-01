ba5232942b4bc9ea786d2ec005844ae3576e44e7
Carlos Armando De Castro <92470129+cadecastro@users.noreply.github.com>
2021-11-30 22:58:10 -0500

    Add files via upload
    Agregado conteo de casos diarios por país y globales.

--- analisis_covid_global.py
+++ analisis_covid_global.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Nov 29 13:27:22 2021
-ANÁLISIS MUERTES COVID-19 GLOBALES
+ANÁLISIS CIFRAS COVID-19 GLOBALES
 Autor: Carlos Armando De Castro (cadecastro.com)
 """
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
+#MUERTES:
 #Importar datos de GitHub:
 covid_global=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
 #Retirar coordenadas:
@@ -18,6 +20,7 @@ covid_global=covid_global.groupby(['Country/Region']).sum()
 covid_global=covid_global.transpose()
 muertes_diarias=covid_global.diff(periods=1,axis=0)
 muertes_diarias.index=pd.to_datetime(muertes_diarias.index,dayfirst=False,yearfirst=False)
+muertes_diarias['Mundo']=muertes_diarias.sum(axis=1)
 pais=str(input('País a graficar muertes diarias: '))
 plt.figure(1)
 plt.bar(muertes_diarias.index,muertes_diarias[pais],color='blue')
@@ -25,10 +28,33 @@ plt.plot(muertes_diarias.index,muertes_diarias[pais].rolling(window =7).mean(),'
 plt.title('Muertes diarias COVID-19 reportadas en '+pais,loc='left')
 plt.title('cadecastro.com',loc='right')
 plt.ylim(0,None)
-muertes_mundo=muertes_diarias.sum(axis=1)
 plt.figure(2)
-plt.bar(muertes_mundo.index,muertes_mundo,color='blue')
-plt.plot(muertes_mundo.index,muertes_mundo.rolling(window =7).mean(),'r')
+plt.bar(muertes_diarias.index,muertes_diarias['Mundo'],color='blue')
+plt.plot(muertes_diarias.index,muertes_diarias['Mundo'].rolling(window =7).mean(),'r')
 plt.title('Muertes diarias COVID-19 global',loc='left')
 plt.title('cadecastro.com',loc='right')
 plt.ylim(0,None)
+#CASOS CONFIRMADOS:
+casos_global=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+#Retirar coordenadas:
+casos_global=casos_global.drop(labels=['Lat','Long'],axis=1)
+#Agrupar por país:
+casos_global=casos_global.groupby(['Country/Region']).sum()
+casos_global=casos_global.transpose()
+casos_diarias=casos_global.diff(periods=1,axis=0)
+casos_diarias.index=pd.to_datetime(casos_diarias.index,dayfirst=False,yearfirst=False)
+casos_diarias['Mundo']=casos_diarias.sum(axis=1)
+plt.figure(3)
+plt.bar(casos_diarias.index,casos_diarias[pais],color='blue')
+plt.plot(casos_diarias.index,casos_diarias[pais].rolling(window =7).mean(),'r')
+plt.title('Casos diarios COVID-19 reportadas en '+pais,loc='left')
+plt.title('cadecastro.com',loc='right')
+plt.ylim(0,None)
+plt.figure(4)
+plt.bar(casos_diarias.index,casos_diarias['Mundo'],color='blue')
+plt.plot(casos_diarias.index,casos_diarias['Mundo'].rolling(window =7).mean(),'r')
+plt.title('Casos diarios COVID-19 global',loc='left')
+plt.title('cadecastro.com',loc='right')
+plt.ylim(0,None)
+print('Letalidad por caso en ',pais,'= ',np.format_float_positional(muertes_diarias[pais].sum()/casos_diarias[pais].sum()*100,precision=2),'%')
+print('Letalidad por caso global= ',np.format_float_positional(muertes_diarias['Mundo'].sum()/casos_diarias['Mundo'].sum()*100,precision=2),'%')
