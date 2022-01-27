e244842c5ac8d2a67f4d68647aa003b58a5682de
Carlos Armando De Castro <92470129+cadecastro@users.noreply.github.com>
2022-01-27 17:28:25 -0500

    Update analisis_covid_global.py

--- analisis_covid_global.py
+++ analisis_covid_global.py
@@ -98,8 +98,8 @@ plt.plot(muertes_diarias.index,muertes_diarias['Israel'].rolling(window =7).mean
 plt.plot(muertes_diarias.index,muertes_diarias['US'].rolling(window =7).mean()/pob2021['PopTotal']['United States of America'],'g')
 plt.plot(muertes_diarias.index,muertes_diarias['South Africa'].rolling(window =7).mean()/pob2021['PopTotal']['South Africa'],'m')
 plt.plot(muertes_diarias.index,muertes_diarias['United Kingdom'].rolling(window =7).mean()/pob2021['PopTotal']['United Kingdom'],'c')
-plt.plot(muertes_diarias.index,muertes_diarias['Brazil'].rolling(window =7).mean()/pob2021['PopTotal']['Brazil'],'k')
-plt.plot(muertes_diarias.index,muertes_diarias['Sweden'].rolling(window =7).mean()/pob2021['PopTotal']['Sweden'],'slategrey')
+plt.plot(muertes_diarias.index,muertes_diarias['Brazil'].rolling(window =7).mean()/pob2021['PopTotal']['Brazil'],'lime')
+plt.plot(muertes_diarias.index,muertes_diarias['India'].rolling(window =7).mean()/pob2021['PopTotal']['India'],'y')
 plt.title('Daily COVID-19 Deaths per Capita',size=15)
 plt.title('cadecastro.com',size=10,loc='right')
 plt.ylabel('Daily Deaths per Capita')
@@ -107,14 +107,14 @@ plt.xlabel('Data source: CSSEGISandData/COVID-19 & https://population.un.org/',s
 plt.grid(True,'both','both')
 plt.ylim(0,None)
 plt.xlim(muertes_diarias.index[0],muertes_diarias.index[len(muertes_diarias.index)-1])
-plt.legend(['Colombia','México','Israel','USA','South Africa','United Kingdom','Brazil','Sweden'])
+plt.legend(['Colombia','México','Israel','USA','South Africa','United Kingdom','Brazil','India'])
 
 #Comparativo países de interés:
 comparativo=pd.DataFrame(data=[muertes_diarias['Colombia'].sum()/pob2021['PopTotal']['Colombia'],muertes_diarias['Mexico'].sum()/pob2021['PopTotal']['Mexico'],
                                muertes_diarias['Israel'].sum()/pob2021['PopTotal']['Israel'],muertes_diarias['US'].sum()/pob2021['PopTotal']['United States of America'],
                                muertes_diarias['South Africa'].sum()/pob2021['PopTotal']['South Africa'],muertes_diarias['United Kingdom'].sum()/pob2021['PopTotal']['United Kingdom'],
-                               muertes_diarias['Brazil'].sum()/pob2021['PopTotal']['Brazil'],muertes_diarias['Sweden'].sum()/pob2021['PopTotal']['Sweden']],
-                         index=['Colombia','México','Israel','USA','South Africa','United Kingdom','Brazil','Sweden'])
+                               muertes_diarias['Brazil'].sum()/pob2021['PopTotal']['Brazil'],muertes_diarias['India'].sum()/pob2021['PopTotal']['India']],
+                         index=['Colombia','México','Israel','USA','South Africa','United Kingdom','Brazil','India'])
 comparativo=comparativo.rename(columns={0:'Deaths per capita'})
 comparativo=comparativo.sort_values(by='Deaths per capita',ascending=False)
 print(comparativo)
@@ -148,3 +148,41 @@ plt.xlabel('Data source: CSSEGISandData/COVID-19 & https://population.un.org/',s
 plt.xticks(rotation=90)
 plt.grid(True,'both','both')
 plt.ylim(0,None)
+
+mov_global=pd.read_csv('https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv',usecols=['date','country_region','residential_percent_change_from_baseline'])
+mov_global['date']=pd.to_datetime(mov_global['date'],yearfirst=True)
+mov_medio=pd.pivot_table(data=mov_global,values='residential_percent_change_from_baseline',index='country_region',aggfunc=np.mean)
+
+mov_latam=pd.DataFrame(data=[mov_medio['residential_percent_change_from_baseline']['Colombia'],mov_medio['residential_percent_change_from_baseline']['Mexico'],
+                             mov_medio['residential_percent_change_from_baseline']['Argentina'],mov_medio['residential_percent_change_from_baseline']['Peru'],
+                             mov_medio['residential_percent_change_from_baseline']['Ecuador'],mov_medio['residential_percent_change_from_baseline']['Panama'],
+                             mov_medio['residential_percent_change_from_baseline']['Brazil'],mov_medio['residential_percent_change_from_baseline']['Chile']],
+                       index=['Colombia','México','Argentina','Perú','Ecuador','Panamá','Brasil','Chile'])
+mov_latam=mov_latam.rename(columns={0:'Cambio movilidad residencial promedio'})
+print(mov_latam)
+comparativo_latam=pd.merge(left=comparativo_latam,right=mov_latam,left_index=True,right_index=True)
+print(comparativo_latam)
+
+
+
+x=comparativo_latam['Cambio movilidad residencial promedio']
+y=comparativo_latam['Muertes per capita']
+correlation_matrix = np.corrcoef(x, y)
+correlation_xy = correlation_matrix[0,1]
+R2 = correlation_xy**2
+R2=str(np.format_float_positional(R2,precision=3))
+d=np.polyfit(x,y,1)
+f= np.poly1d(d)
+y1=f(x)
+fig, ax = plt.subplots(figsize=(12,6))
+plt.plot(x,y,'bo')
+plt.plot(x,y1,'r')
+plt.title('Muertes per capita COVID-19 vs. Movilidad hogar en Latinoamérica - R²='+R2,size=12,loc='left')
+plt.title('cadecastro.com',size=10,loc='right')
+plt.ylabel('Muertes/Habitantes')
+plt.xlabel('Cambio en movilidad de hogares (%)',size=10)
+plt.xticks(rotation=90)
+plt.grid(True,'both','both')
+plt.ylim(0,None)
+for index in range(len(x)):
+  ax.text(x[index], y[index], comparativo_latam.index[index], size=10)
